9a01216551bfd165a9cb2126933c993aa66d13c7
Razvan Rotundu <raz.rotundu@gmail.com>
2025-04-28 16:18:36 -0400

    Connect interpreter to main Lox pipeline
    Lox.run(): scanning -> parsing -> execution

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -10,6 +10,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+	private static final Interpreter interpreter = new Interpreter();
 	static boolean hadError = false;
 	static boolean hadRuntimeError = false;
 	
@@ -77,7 +78,8 @@ public class Lox {
 		
 		if(hadError) return;
 		
-		System.out.println(new AstPrinter().print(expression));
+//		System.out.println(new AstPrinter().print(expression));
+		interpreter.interpret(expression);
 	}
 	
 	/**
